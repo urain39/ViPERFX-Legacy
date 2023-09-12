1d5cb16e7be6b644a0c2c7f54d98dbd39f0f7139
urain39 <urain39@qq.com>
2023-09-12 13:06:41 +0800

    rename some vars

--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
 CONFIG_FILE="/system/etc/audio_effects.conf"
 VENDOR_CONFIG_FILE="/system/vendor/etc/audio_effects.conf"
-AUDIO_POLICY="/system/etc/audio_policy.conf"
+AUDIO_POLICY_FILE="/system/etc/audio_policy.conf"
 
 
 make_copy() {
@@ -12,7 +12,7 @@ make_copy() {
 
 make_copy "$CONFIG_FILE"
 make_copy "$VENDOR_CONFIG_FILE"
-make_copy "$AUDIO_POLICY"
+make_copy "$AUDIO_POLICY_FILE"
 
 
 sed -i \
@@ -21,4 +21,4 @@ sed -i \
   -e 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' \
   -e 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' \
   "$MODPATH$CONFIG_FILE" "$MODPATH$VENDOR_CONFIG_FILE"
-sed -i '/deep_buffer {/,/}/d' "$MODPATH$AUDIO_POLICY"
+sed -i '/deep_buffer {/,/}/d' "$MODPATH$AUDIO_POLICY_FILE"

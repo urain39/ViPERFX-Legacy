28876dc3527b42389ad1344aab77698e11408837
urain39 <urain39@qq.com>
2023-09-13 15:32:02 +0800

    update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,12 +7,13 @@ make_copy() {
   local d="$MODPATH${1%/*}"
   mkdir -p "$d"
   cp -p "$1" "$d"
+  echo "$MODPATH$1"
 }
 
 
-make_copy "$CONFIG_FILE"
-make_copy "$VENDOR_CONFIG_FILE"
-make_copy "$AUDIO_POLICY_FILE"
+CONFIG_FILE="$(make_copy "$CONFIG_FILE")"
+VENDOR_CONFIG_FILE="$(make_copy "$VENDOR_CONFIG_FILE")"
+AUDIO_POLICY_FILE="$(make_copy "$AUDIO_POLICY_FILE")"
 
 
 sed -i \
@@ -20,5 +21,5 @@ sed -i \
   -e '/v4a_standard_fx {/,/}/d' \
   -e 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' \
   -e 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' \
-  "$MODPATH$CONFIG_FILE" "$MODPATH$VENDOR_CONFIG_FILE"
-sed -i '/deep_buffer {/,/}/d' "$MODPATH$AUDIO_POLICY_FILE"
+  "$CONFIG_FILE" "$VENDOR_CONFIG_FILE"
+sed -i '/deep_buffer {/,/}/d' "$AUDIO_POLICY_FILE"
